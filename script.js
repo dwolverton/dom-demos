4efea3674d88949806232fd9f994e5afb7e7861f
David Wolverton <dwolvert@gmail.com>
2022-05-12 19:24:06 -0400

    DOM demo

--- script.js
+++ script.js
@@ -4,7 +4,17 @@
   document.getElementById("actionButton").addEventListener("click", main);
 
   function demo() {
-    
+    // step 1 access it
+    let demoButton = document.getElementById("demoButton");
+    // step 2 modify it
+    demoButton.innerText = "Hello <em>World</em>";
+    demoButton.innerHTML = "Hello <em>World</em>";
+
+    // step 1 access it
+    let shrinkText = document.getElementById("shrink-me");
+    // step 2 modify it
+    // shrinkText.style.color = "orange";
+    shrinkText.classList.add("orange");
   }
 
   function main() {
