f8d465ccebdc1bb2c3772eae32612e4798458670
David Wolverton <dwolvert@gmail.com>
2022-05-12 14:29:55 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+"use strict";
+{
+  document.getElementById("demoButton").addEventListener("click", demo);
+  document.getElementById("actionButton").addEventListener("click", main);
+
+  function demo() {
+    
+  }
+
+  function main() {
+    // We'll write code together here...
+  }
+
+}
