078df23fd498d1cc3a178342fa5a0e95b3c9fe62
David Wolverton <dwolvert@gmail.com>
2022-05-12 21:31:28 -0400

    Stuff from David

--- events-demo/script.js
+++ events-demo/script.js
@@ -7,3 +7,10 @@ clickMeButton.addEventListener("click", () => {
 
   document.body.style.backgroundColor = "black";
 });
+
+document.body.addEventListener("mousemove", (event) => {
+  // console.log(event);
+  const circle = document.getElementById("circle");
+  circle.style.left = `${event.pageX - 50}px`;
+  circle.style.top = `${event.pageY - 50}px`;
+});
